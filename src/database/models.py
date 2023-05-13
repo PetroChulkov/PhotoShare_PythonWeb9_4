321342cdbcf9e74a381ab7b0fc0315590db8e20e
SergiiGoncharov13 <goncharov.s.work@gmail.com>
2023-05-13 22:15:57 +0300

    fix

--- src/database/models.py
+++ src/database/models.py
@@ -22,16 +22,15 @@ class User(Base):
     refresh_token = Column(String(255), nullable=True)
     roles = Column("role", Enum(Role), default=Role.user)
     confirmed = Column(Boolean, default=False)
-    user_id = Column("user_id", ForeignKey("users.id", ondelete="CASCADE"))
-    user = relationship("User", backref="students", innerjoin=True)
+
 
 
 class Photo(Base):
     __tablename__ = "photos"
     id = Column(Integer, primary_key=True)
     description = Column(String(200))
-    user_id = Column(Integer(), ForeignKey("users.id"))
-    photo_user = relationship("User", back_populates="photo")
+    user_id = Column("user_id", ForeignKey("users.id", ondelete="CASCADE"))
+    user = relationship("User", backref="students", innerjoin=True)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
 
