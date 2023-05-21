a66792b67eddef0649186651ad7e35a00d79daec
LevytskyiS <levytskyi.sv@gmail.com>
2023-05-21 12:03:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ def healthchecker(db: Session = Depends(get_db)):
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="Database is not configured correctly",
             )
-        return {"message": "Welcome to FastAPI!"}
+        return {"message": "Welcome to PhotoShare4!"}
     except Exception as e:
         logger.error(e)
         raise HTTPException(
